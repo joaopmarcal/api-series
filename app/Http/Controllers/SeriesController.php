0ca882806d28355d374ffa61bcee1aba4a79c327
João Paulo <joaopaulozangrande@gmail.com>
2021-02-18 18:22:30 -0300

    first route

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController
+{
+    public function index()
+    {
+        return [
+            "Grey's Anatomy",
+            "Lost"
+        ];
+    }
+}
