ab8a3f92da72460b4accee40f0ff438bb6612ddb
João Paulo <joaopaulozangrande@gmail.com>
2021-02-18 18:46:38 -0300

    I mapped my database.

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
+
 class SeriesController
 {
     public function index()
     {
-        return [
-            "Grey's Anatomy",
-            "Lost"
-        ];
+        return Serie::all();
     }
 }
