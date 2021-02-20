aacabffd3e29315a1d097db5213a39db1e78784f
João Paulo <joaopaulozangrande@gmail.com>
2021-02-20 09:02:38 -0300

    Abstract Controller.

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -3,55 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Episodio;
-use Illuminate\Http\Request;
 
-class EpisodiosController
+class EpisodiosController extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return Episodio::all();
-    }
-
-    public function store(Request $request)
-    {
-        return response()
-            ->json(
-                Episodio::create($request->all()),
-                201
-            );
-    }
-
-    public function show(int $id)
-    {
-        $serie = Episodio::find($id);
-        if (is_null($serie)){
-            return response()->json('', 204);
-        }
-        return response()->json($serie);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Episodio::find($id);
-        if (is_null($serie)){
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ], 404);
-        }
-        $serie->fill($request->all());
-        $serie->save();
-
-        return $serie;
-    }
-
-    public function destroy(int $id)
-    {
-        $qtdRecursosRemovidos = Episodio::destroy($id);
-        if ($qtdRecursosRemovidos === 0){
-            return response()->json([
-                'erro' => 'Recurso não encontrado'
-            ], 404);
-        }
-        return response()->json('', 204);
+        $this->classe = Episodio::class;
     }
 }
